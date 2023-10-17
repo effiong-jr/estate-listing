7cbb05f2a6e9f4dfc4593cc34ec4f210fccf3784
Effiong Bassey <effiong.bassey@arm.com.ng>
2023-10-17 01:19:17 +0100

    Create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+
 const App = () => {
-  console.log(1 + 2);
-  return <div className="text-red-500">Real Estate application</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
