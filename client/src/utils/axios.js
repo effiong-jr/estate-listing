57e0e8927b93e02bb9012e5912a0b4baf7850abf
Effiong Bassey <effiong.bassey@arm.com.ng>
2023-12-04 00:41:44 +0100

    Added axios interceptor and cleared localStorage on 401 response

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -6,4 +6,23 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
+axiosInstance.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response) {
+      if (error.response.status === 401) {
+        localStorage.clear();
+        window.location.replace("/sign-in");
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
